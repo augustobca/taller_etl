94ee19dea5d6f878cb289ef17ee8be30b2bfc214
augustobca <augusto.correa@unl.edu.ec>
2026-03-18 15:43:18 -0500

    🚀 Proyecto ETL: Subida inicial limpia y segura

---
+++ dbt/proyecto_unl/models/example/stg_calidad_aire.sql
@@ -0,0 +1,16 @@
+WITH source_data AS (
+    -- El primer parámetro es el 'name' del source en el YAML
+    -- El segundo es el 'name' de la tabla (nombre del archivo seed sin .csv)
+    SELECT * FROM {{ source('fuentes_unl', 'calidad_aire') }}
+),
+
+renamed AS (
+    SELECT
+        ciudad_id AS nombre_ciudad, -- Renombrado para coincidir con clima
+        indice_ica,
+        particulas_pm25,
+        NOW() AS fecha_carga
+    FROM source_data
+)
+
+SELECT * FROM renamed
